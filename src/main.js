2bf5c69afa2212d8d4da56578a912832c77718a9
MemoCSales <memocsales@gmail.com>
2025-01-10 16:45:51 +0100

    fix: instead of link element use script to process the styles.css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import './styles/styles.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
