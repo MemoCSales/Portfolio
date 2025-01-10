f5a34bf0d06b0f1a7ecdba612ce7d82db47223fd
MemoCSales <memocsales@gmail.com>
2025-01-10 14:19:38 +0100

    changing font and fixing bugs in mobile screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,25 @@ export default {
       'desktop': '1280px',
       // => @media (min-width: 1280px) { ... }
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        'mono': ["Ubuntu Mono", 'monospace'],
+      },
+      colors: {
+        'primary': '#171717',
+        'secondary': '#262626',
+        'accent': '#64ffda',
+        'text': '#9ca3af',
+        'text-light': '#f3f4f6'
+      },
+      spacing: {
+        'section': '100vh'
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-in',
+        'slide-up': 'slideUp 0.5s ease-out'
+      }
+    },
   },
   plugins: [],
 };
